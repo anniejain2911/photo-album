77ddf8724953d7d035a91b00d93882b68f7653d2
Annie Jain <anniejain07@gmail.com>
2025-12-04 23:35:09 -0500

    commit

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   REGION: 'us-west-2',
-  BUCKET: 'photo-album-site-cf-tm1204-481207240584-us-west-2',
-  API_ROOT: 'https://wnf9tlc97e.execute-api.us-west-2.amazonaws.com/prod',
+  BUCKET: 'photo-album--photos-b2',
+  API_ROOT: 'https://tuwhjyekq7.execute-api.us-west-2.amazonaws.com/prod',
   API_KEY: 'iLGCf6L9tV1UsDFroyA9G12zRTCHu8OU1t8hxmOa' 
 };
