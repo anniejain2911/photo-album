e3a4db58d04e546714b9c6ef7e24f3cb334d02bb
Annie Jain <anniejain07@gmail.com>
2025-12-04 22:51:41 -0500

    apikey

--- frontend/config.js
+++ frontend/config.js
@@ -2,5 +2,5 @@ window.APP_CONFIG = {
   REGION: 'us-west-2',
   BUCKET: 'photo-album-site-cf-tm1204-481207240584-us-west-2',
   API_ROOT: 'https://wnf9tlc97e.execute-api.us-west-2.amazonaws.com/prod',
-  API_KEY: '' 
+  API_KEY: 'iLGCf6L9tV1UsDFroyA9G12zRTCHu8OU1t8hxmOa' 
 };
